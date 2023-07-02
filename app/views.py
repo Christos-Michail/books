629536004c914e882fcb82cabf7af9885989a612
christos michail <christos.m.michael@gmail.com>
2023-07-03 01:40:49 +0300

    adding CRUD views of book API

--- app/views.py
+++ app/views.py
@@ -1,4 +1,3 @@
-import os
 from . import create_app, db
 from .models import Book, Author, Work
 from flask import jsonify, request, abort
@@ -11,6 +10,7 @@ with app.app_context():
 OPEN_LIBRARY="https://openlibrary.org"
 BOOKS_API = "https://openlibrary.org/books/"
 WORKS_API = "https://openlibrary.org/works/"
+
 @app.route("/book/favourites", methods=["GET"])
 def get_favourite_books():
     favourite_books=['OL7353617M', 'OL33145448M', 'OL1017798M']
@@ -45,3 +45,44 @@ def get_favourite_books():
             db.session.commit()
     books = Book.query.all()
     return jsonify([book.to_json() for book in books])
+
+@app.route("/book/<string:key>", methods=["GET"])
+def get_book(key):
+    print(key)
+    book = Book.query.get(key)
+    if book is None:
+        abort(404, 'make sure you use book/favourites first!')
+    return jsonify(book.to_json())
+
+@app.route("/book/<string:key>", methods=["DELETE"])
+def delete_book(key):
+    book = Book.query.get(key)
+    if book is None:
+        abort(404, 'make sure you use book/favourites first!')
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'result': True})
+
+@app.route("/book", methods=['POST'])
+def create_book():
+    if not request.json:
+        abort(400,'Failed, please check your request is json')
+    book = Book(key=request.json.get('key'),
+        title=request.json.get('title'),
+        number_of_pages=request.json.get('number_of_pages')
+    )
+    db.session.add(book)
+    db.session.commit()
+    return jsonify(book.to_json()), 201
+
+@app.route('/book/<string:key>', methods=['PUT'])
+def update_book(key):
+    if not request.json:
+        abort(400)
+    book = Book.query.get(key)
+    if book is None:
+        abort(404,'Not found !')
+    book.title = request.json.get('title', book.title)
+    book.number_of_pages = request.json.get('number_of_pages', book.number_of_pages)
+    db.session.commit()
+    return jsonify(book.to_json())
